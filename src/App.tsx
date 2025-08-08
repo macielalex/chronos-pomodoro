766ca4ad3a1d76be8b40b817360ff02585cfb918
Alex Maciel <alexmaciel.mail@gmail.com>
2025-08-07 22:38:32 -0300

    ajuste do heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import './styles/theme.css';
 import './styles/global.css';
 import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>Olá Mundo</Heading>
+      <Heading>
+        Olá Mundo{' '}
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
 
       <p>Bem-vindo à minha aplicação React</p>
     </>
